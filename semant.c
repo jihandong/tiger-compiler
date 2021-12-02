2ff586d1fff96fea767c3f019a2cc84417f7b318
jihandong <jihandong@xiaomi.com>
2021-12-02 20:33:30 +0800

    stash today's semantic check

--- semant.c
+++ semant.c
@@ -11,17 +11,22 @@
  * Definitions
  ********************************************************************************/
 
-typedef void *ir_exp; /*< ir not implemented yet */
+typedef void *I_exp; /*< ir not implemented yet */
 
-static struct sem_exp { ir_exp exp; ty_type type; };
+typedef struct T_exp_ T_exp;
+
+struct T_exp_ {
+    I_exp  exp;
+    T_type type;
+};
 
 /********************************************************************************
  * Privates
  ********************************************************************************/
 
-static sem_exp sem_mk_exp(ir_exp exp, ty_type type)
+static T_exp T_mk_exp(I_exp exp, T_type type)
 {
-    struct sem_exp e;
+    T_exp e;
 
     e.exp   = exp;
     e.type  = type;
@@ -33,12 +38,12 @@ static sem_exp sem_mk_exp(ir_exp exp, ty_type type)
  * Publics
  ********************************************************************************/
 
-void sem_trans_dec(symbol_table venv, symbol_table tenv, ast_var n)
+void T_trans_dec(S_table venv, S_table tenv, A_var n)
 {
     switch(n->kind) {
-        case kind_dec_var: {
-            struct sem_exp init = sem_trans_exp(venv, tenv, n->u.var.init);
-            ty_type var_type;
+        case A_kind_dec_var: {
+            T_exp init = T_trans_exp(venv, tenv, n->u.var.init);
+            T_type var_type;
 
             // require type match between 'type' and 'init'.
             if (n->u.var.type) {
@@ -50,17 +55,17 @@ void sem_trans_dec(symbol_table venv, symbol_table tenv, ast_var n)
             return;
         }
 
-        case kind_dec_type:
-            sym_table_add(tenv, n->u.type.type_s, sem_trans_type(n->u.type.type));
+        case A_kind_dec_type:
+            sym_table_add(tenv, n->u.type.type_s, T_trans_type(n->u.type.type));
             return;
 
-        case kind_dec_func: {
-            ty_type ret_type = sym_table_get(n->u.func.ret);
-            ty_type_list arg_type_list = NULL; /* ast_tfield_list -> ty_type_list */
-            ty_type_list t;
-            ast_tfield_list a;
+        case A_kind_dec_func: {
+            T_type ret_type = sym_table_get(n->u.func.ret);
+            T_type_list arg_type_list = NULL; /* A_para_list -> T_type_list */
+            T_type_list t;
+            A_para_list a;
 
-            sym_table_add(venv, n->u.func.func, ty_mk_func(ret_type, arg_type_list));
+            sym_table_add(venv, n->u.func.func, T_mk_func(ret_type, arg_type_list));
 
             // type check in function body.
             sym_table_begin(venv);
@@ -78,82 +83,81 @@ void sem_trans_dec(symbol_table venv, symbol_table tenv, ast_var n)
     }
 }
 
-struct sem_exp sem_trans_exp(symbol_table venv, symbol_table tenv, ast_exp n)
+T_exp T_trans_exp(S_table venv, S_table tenv, A_exp n)
 {
     switch(n->kind) {
-        case kind_exp_var:
-            return sem_trans_var(venv, tenv, n->u.var);
+        case A_kind_exp_var:
+            return T_trans_var(venv, tenv, n->u.var);
 
-        case kind_exp_nil:
-            return sem_mk_exp(NULL, ty_mk_nil());
+        case A_kind_exp_nil:
+            return T_mk_exp(NULL, T_mk_nil());
 
-        case kind_exp_int:
-            return sem_mk_exp(NULL, ty_mk_int());
+        case A_kind_exp_int:
+            return T_mk_exp(NULL, T_mk_int());
 
-        case kind_exp_string:
-            return sem_mk_exp(NULL, ty_mk_string());
+        case A_kind_exp_string:
+            return T_mk_exp(NULL, T_mk_string());
 
-        case kind_exp_call:
-            
+        case A_kind_exp_call:
 
-        case kind_exp_op: {
-            struct sem_exp left  = sem_trans_exp(venv, tenv, n->u.op.left);
-            struct sem_exp right = sem_trans_exp(venv, tenv, n->u.op.right);
+        case A_kind_exp_op: {
+            T_exp left  = T_trans_exp(venv, tenv, n->u.op.left);
+            T_exp right = T_trans_exp(venv, tenv, n->u.op.right);
 
-            if (left.type->kind != kind_ty_int)
+            if (left.type->kind != T_kind_int)
                 util_err(n->u.op.left->pos, "exp op: left exp's type not match");
-            if (right.type->kind != kind_ty_int)
+            if (right.type->kind != T_kind_int)
                 util_err(n->u.op.right->pos, "exp op: right exp's type not match");
 
-            return sem_mk_exp(NULL, ty_mk_int());
+            return T_mk_exp(NULL, T_mk_int());
         }
 
-        case kind_exp_record:
+        case A_kind_exp_record:
             
 
-        case kind_exp_seq: {
-            struct sem_exp last;
-            ast_exp_list list;
+        case A_kind_exp_seq: {
+            T_exp last;
+            A_exp_list list;
 
-            for(list = n->u.seq, last = NULL; list; list = list->next)
-                last = sem_trans_exp(venv, tenv, list->head);
+            for(list = n->u.seq, last = NULL; list; list = list->tail)
+                last = T_trans_exp(venv, tenv, list->head);
 
-            return sem_mk_exp(NULL, last);
+            return last;
         }
 
-        case kind_exp_assign:
+        case A_kind_exp_assign:
             
 
-        case kind_exp_if:
-            struct sem_exp cond   = sem_trans_exp(venv, tenv, n->u.if_.cond);
-            struct sem_exp then   = sem_trans_exp(venv, tenv, n->u.if_.then);
-            struct sem_exp else_  = sem_trans_exp(venv, tenv, n->u.if_.else_);
+        case A_kind_exp_if:
+            T_exp cond   = T_trans_exp(venv, tenv, n->u.if_.cond);
+            T_exp then   = T_trans_exp(venv, tenv, n->u.if_.then);
+            T_exp else_  = T_trans_exp(venv, tenv, n->u.if_.else_);
 
-            if (cond.type != kind_ty_int)
+            if (cond.type != T_kind_int)
                 util_err(n->u.op.left->pos, "exp if: cond exp's type isnot int");
 
             /* what should I return? */
 
-        case kind_exp_while:
+        case A_kind_exp_while:
             
 
-        case kind_exp_for:
+        case A_kind_exp_for:
             
 
-        case kind_exp_break:
-            return sem_mk_exp(NULL, ty_mk_void());
+        case A_kind_exp_break:
+            return T_mk_exp(NULL, T_mk_void());
 
-        case kind_exp_let: {
-            struct sem_exp exp;
-            ast_dec_list d;
+        case A_kind_exp_let: {
+            T_exp exp;
+            A_dec_list d;
 
             // prepare decs
             sym_table_begin(venv);
             sym_table_begin(tenv);
 
-            for(d = n->u.let.decs; d; d = d->next)
-                sem_trans_dec(venv, tenv, d->head);
-            exp = sem_trans_exp(venv, tenv, n->u.let.body);
+            for(d = n->u.let.decs; d; d = d->tail)
+                T_trans_dec(venv, tenv, d->head);
+            exp = T_trans_exp(venv, tenv, n->u.let.body);
 
             sym_table_end(tenv);
             sym_table_end(venv);
@@ -162,14 +166,14 @@ struct sem_exp sem_trans_exp(symbol_table venv, symbol_table tenv, ast_exp n)
             return exp;
         }
 
-        case kind_exp_array:
+        case A_kind_exp_array:
             
 
         default:
     }
 }
 
-struct sem_exp sem_trans_var(symbol_table venv, symbol_table tenv, ast_var n)
+T_exp T_trans_var(S_table venv, S_table tenv, A_var n)
 {
     switch(n->kind) {
         case:
@@ -177,7 +181,7 @@ struct sem_exp sem_trans_var(symbol_table venv, symbol_table tenv, ast_var n)
     }
 }
 
-ty_type sem_exp sem_trans_type(symbol_table tenv, ast_type n)
+T_type T_exp T_trans_type(S_table tenv, A_type n)
 {
     switch(n->kind) {
         case:
